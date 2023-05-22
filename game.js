8d6915932766bfd69714d79caa41eb47a3e5f867
Iron <marco.jan@gmx.de>
2023-05-22 11:35:16 +0200

    init commit

--- game.js
+++ game.js
@@ -2,27 +2,29 @@ let count_player = 0;
 let count_npc = 0;
 let counter = 0;
 
-function computerplay() {
+function player(){
     console.log("1 für Schere, 2 für Stein, 3 für Papier");
     let input = prompt("Bitte geben Sie eine Zahl ein:");
-
     input = parseInt(input);
+    return [input]
+}
 
+function computerplay() {
     let npc = Math.floor(Math.random() * 3) + 1;
-    // Rückgabe der Eingabe
-    return [input, npc];
+    return [npc];
 }
 
 function playround() {
-    const [input, npc] = computerplay();
-    console.log(counter)
+    const [input] = player();
+    const [npc] = computerplay();
+    //console.log(counter)
     if (input === 1) {
         if (input + npc === 3) {
             console.log("Schere verliert gegen Stein. Sie haben verloren"); count_npc++; counter++;
         } else if (input + npc === 4) {
             console.log("Schere gewinnt gegen Papier. Sie haben gewonnen"); count_player++; counter++;
         } else {
-            console.log("Unentschieden, nochmal");counter++;
+            console.log("Unentschieden, nochmal"); counter++;
         }
     } else if (input === 2) {
         if (input + npc === 4) {
@@ -30,15 +32,15 @@ function playround() {
         } else if (input + npc === 5) {
             console.log("Stein verliert gegen Papier. Sie haben verloren"); count_npc++; counter++;
         } else {
-            console.log("Unentschieden, nochmal");counter++;
+            console.log("Unentschieden, nochmal"); counter++;
         }
-    } else if (input === 3) {
+    } else if (input === 3) {const [input, npc] = computerplay();
         if (input + npc === 4) {
             console.log("Papier verliert gegen Schere. Sie haben verloren"); count_npc++; counter++;
         } else if (input + npc === 5) {
             console.log("Papier gewinnt gegen Stein. Sie haben gewonnen"); count_player++; counter++;
         } else {
-            console.log("Unentschieden, nochmal");counter++;
+            console.log("Unentschieden, nochmal"); counter++;
         }
     }
     console.log("Aktueller Spielstand: Spieler " + count_player + " - " + count_npc + " NPC");
@@ -49,7 +51,19 @@ function game() {
         playround();
     }
 }
-game();
 
+game();
+winner();
+
+function winner() {
+    if (count_player > count_npc) {
+        console.log("Spieler hat gewonnen")
+    } else if (count_npc > count_player) {
+        console.log("NPC hat gewonnen")
+    }
+    else {
+        console.log("Das Spiel ging unendschieden aus")
+    }
+}
 console.log("Spiel beendet");
 console.log("Endstand: Spieler " + count_player + " - " + count_npc + " NPC");
